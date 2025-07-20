84afdc556411feaa3597c89fbc3092a75c690ef0
Md Ekramul Haque <138347323+ekramul28@users.noreply.github.com>
2025-07-20 10:23:42 +0600

    added fontend url

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,15 @@ const app: Application = express();
 app.use(express.json());
 // app.use(cookieParser());
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mini-event-scheduler-with-ai-catego-seven.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 // application routes
 app.use("/api/v1", router);
